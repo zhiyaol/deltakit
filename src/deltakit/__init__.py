1df3056fd06bcb65052bb8adb82f31b69ffd5b46
Adrien Suau <12374487+nelimee@users.noreply.github.com>
2025-09-17 14:54:45 +0100

    fix: Add the Client class to the re-exports in deltakit (#11)

--- src/deltakit/__init__.py
+++ src/deltakit/__init__.py
@@ -3,5 +3,10 @@ import importlib.metadata
 
 __version__ = importlib.metadata.version(__package__)
 
+from deltakit_explorer import Client
+
 # Prevent import of `importlib` (and any other non-public objects) from this module.
 del importlib
+
+# List only public members in `__all__`.
+__all__ = [s for s in dir() if not s.startswith("_")]

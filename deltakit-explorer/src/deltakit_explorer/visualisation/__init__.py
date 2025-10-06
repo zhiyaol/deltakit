bade0255483a7c1785e7baf0dfc9d35779fe8631
Zhiyao Li <40032538+zhiyaol@users.noreply.github.com>
2025-10-06 14:35:10 -0700

    move visualization code into existing visualisation moduel

--- deltakit-explorer/src/deltakit_explorer/visualisation/__init__.py
+++ deltakit-explorer/src/deltakit_explorer/visualisation/__init__.py
@@ -3,7 +3,9 @@
 
 from deltakit_explorer.visualisation._visualisation import (correlation_matrix,
                                                             defect_diagram,
-                                                            defect_rates)
+                                                            defect_rates,
+                                                            plot_leppr,
+                                                            plot_lambda)
 
 # List only public members in `__all__`.
 __all__ = [s for s in dir() if not s.startswith("_")]
